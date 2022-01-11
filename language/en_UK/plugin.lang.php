c15cd3e81b57d24eec8f09b8895be887afc8cf0d
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-11 16:47:51 +0100

    translated
    translated but dont show

---
+++ language/en_UK/plugin.lang.php
@@ -0,0 +1,4 @@
+<?php
+$lang['Check Uploads'] = 'Check Uploads';
+$lang['Check upload directory'] = 'Check upload directory';
+?>
---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,4 @@
+<?php
+$lang['Check Uploads'] = 'Preverite Prenose';
+$lang['Check upload directory'] = 'Preverite imenik za prenose';
+?>
